34ef9e6cfad4030ffe9754a0bec0287474f5bff3
Roman H <roman.heinrich@gmail.com>
2012-05-31 16:33:21 +0300

    ssl_certs.sh is more robust now (/usr/bin/env bash) (had some probs in ubuntu 12.04)

--- ssl_certs.sh
+++ ssl_certs.sh
@@ -1,41 +1,40 @@
-#!/bin/sh
+#!/usr/bin/env bash
 function clean {
-rm -f ./server_*
-rm -f ./client_*
-rm -f ./testca/cacert*
-rm -f ./testca/index.txt.*
-rm -f ./testca/private/*
-rm -f ./testca/serial.old
-rm -f ./testca/certs/*
-rm -f ./testca/index.txt
-rm -f ./testca/serial
+  rm -f ./server_*
+  rm -f ./client_*
+  rm -f ./testca/cacert*
+  rm -f ./testca/index.txt.*
+  rm -f ./testca/private/*
+  rm -f ./testca/serial.old
+  rm -f ./testca/certs/*
+  rm -f ./testca/index.txt
+  rm -f ./testca/serial
 }
 
 function generate {
-mkdir -p testca/private
-mkdir -p testca/certs
-touch testca/index.txt
-echo 01 > testca/serial
-cd testca
-openssl req -x509 -config ../openssl.cnf -newkey rsa:2048 -days 40000 -out cacert.pem -outform PEM -subj /CN=TestCA/ -nodes
-openssl x509 -in cacert.pem -out cacert.cer -outform DER
-cd ..
-openssl genrsa -out server_key.pem 2048
-openssl req -new -key server_key.pem -out server_req.pem -outform PEM -subj /CN=$(hostname)/O=server/ -nodes
-cd testca
-openssl ca -config ../openssl.cnf -in ../server_req.pem -out ../server_cert.pem -notext -batch -extensions server_ca_extensions
-cd ..
-openssl pkcs12 -export -out server_keycert.p12 -in server_cert.pem -inkey server_key.pem -passout pass:DemoPass
-openssl genrsa -out client_key.pem 2048
-openssl req -new -key client_key.pem -out client_req.pem -outform PEM -subj /CN=$(hostname)/O=client/ -nodes
-cd testca
-openssl ca -config ../openssl.cnf -in ../client_req.pem -out ../client_cert.pem -notext -batch -extensions client_ca_extensions
-cd ..
-openssl pkcs12 -export -out client_keycert.p12 -in client_cert.pem -inkey client_key.pem -passout pass:DemoPass
-
+  mkdir -p testca/private
+  mkdir -p testca/certs
+  touch testca/index.txt
+  echo 01 > testca/serial
+  cd testca
+  openssl req -x509 -config ../openssl.cnf -newkey rsa:2048 -days 40000 -out cacert.pem -outform PEM -subj /CN=TestCA/ -nodes
+  openssl x509 -in cacert.pem -out cacert.cer -outform DER
+  cd ..
+  openssl genrsa -out server_key.pem 2048
+  openssl req -new -key server_key.pem -out server_req.pem -outform PEM -subj /CN=$(hostname)/O=server/ -nodes
+  cd testca
+  openssl ca -config ../openssl.cnf -in ../server_req.pem -out ../server_cert.pem -notext -batch -extensions server_ca_extensions
+  cd ..
+  openssl pkcs12 -export -out server_keycert.p12 -in server_cert.pem -inkey server_key.pem -passout pass:DemoPass
+  openssl genrsa -out client_key.pem 2048
+  openssl req -new -key client_key.pem -out client_req.pem -outform PEM -subj /CN=$(hostname)/O=client/ -nodes
+  cd testca
+  openssl ca -config ../openssl.cnf -in ../client_req.pem -out ../client_cert.pem -notext -batch -extensions client_ca_extensions
+  cd ..
+  openssl pkcs12 -export -out client_keycert.p12 -in client_cert.pem -inkey client_key.pem -passout pass:DemoPass
 }
 
-if [ "$1" = "generate" ]; then 
+if [ "$1" = "generate" ]; then
   echo "Generating ssl certificates..."
   generate
   exit
